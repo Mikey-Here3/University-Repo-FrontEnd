cbd7033b9bfdfdd06489d559d2006343eb1ff780
Mikey <135088339+Mikey-Here3@users.noreply.github.com>
2026-03-12 16:17:05 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,13 @@ export const metadata: Metadata = {
       "NUML Past Papers 2024–2026 | StudyHouse – Midterm & Final Papers Download",
     template: "%s | StudyHouse NUML",
   },
-
+icons: {
+  icon: [
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
   description: siteConfig.description,
 
   keywords: [
